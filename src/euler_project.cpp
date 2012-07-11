95718a7e72dd260e7ea2a7c97faafa4ef70785ac
Fajrian Yunus <fajrian.yunus@gmail.com>
2012-07-11 22:57:22 +0800

    initial commit
    Signed-off-by: Fajrian Yunus <fajrian.yunus@gmail.com>

---
+++ src/euler_project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : euler_project.cpp
+// Author      : Fajrian Yunus
+// Version     :
+// Copyright   : Fajrian Yunus' copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
